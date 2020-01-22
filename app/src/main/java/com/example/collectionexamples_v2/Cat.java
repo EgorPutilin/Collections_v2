2fd47577770a92e45739a4b7ccbbe67326aa8ed7
EgorPutilin <59206800+EgorPutilin@users.noreply.github.com>
2020-01-22 18:56:06 +0300

    commit

---
+++ app/src/main/java/com/example/collectionexamples_v2/Cat.java
@@ -0,0 +1,18 @@
+package com.example.collectionexamples_v2;
+
+public class Cat {
+    private String name;
+
+
+    public Cat() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/collectionexamples_v2/MainActivity.java
+++ app/src/main/java/com/example/collectionexamples_v2/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Cat cat = new Cat();
+    }
+
+    private void print2(ArrayList<Cat> cats) {
+        for (Cat cat: cats) {
+            System.out.println("Cat name: " + cat.getName());
+        }
     }
 }
